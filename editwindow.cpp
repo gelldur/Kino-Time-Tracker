f9009eca96d1060a994df6e2be3d7dd483c92599
dajvido <jedynywierny@gmail.com>
2012-11-30 10:02:13 +0100

    #15 Changed QRegExp for years

--- editwindow.cpp
+++ editwindow.cpp
@@ -13,30 +13,22 @@ EditWindow::EditWindow(QWidget *parent) :
     ui->setupUi(this);
 
     QRegExp regExpDay("0[1-9]|[12][0-9]|3[01]");
-    ui->FDay->setValidator(new QRegExpValidator(regExpDay, this));
-    ui->TDay->setValidator(new QRegExpValidator(regExpDay, this));
+    ui->fDay->setValidator(new QRegExpValidator(regExpDay, this));
+    ui->tDay->setValidator(new QRegExpValidator(regExpDay, this));
 
     QRegExp regExpMonth("0[1-9]|1[012]");
-    ui->FMonth->setValidator(new QRegExpValidator(regExpMonth, this));
-    ui->TMonth->setValidator(new QRegExpValidator(regExpMonth, this));
+    ui->fMonth->setValidator(new QRegExpValidator(regExpMonth, this));
+    ui->tMonth->setValidator(new QRegExpValidator(regExpMonth, this));
 
     QString stringYear = QDateTime::currentDateTime().toString("yyyy");
-    char buffor[256];
-    sprintf(buffor,"(19)[0-9]{2}|[2-%c][0-%c][0-%c][0-%c]",
-            stringYear.toStdString()[0],
-            stringYear.toStdString()[1],
-            stringYear.toStdString()[2],
-            stringYear.toStdString()[3]);
-
-    qDebug("%s\n",buffor);
-
-
-    QRegExp regExpYear(buffor);
-    ui->FYear->setValidator(new QRegExpValidator(regExpYear, this));
-    ui->TYear->setValidator(new QRegExpValidator(regExpYear, this));
+    QRegExp regExpYear(stringYear);
+    ui->fYear->setValidator(new QRegExpValidator(regExpYear, this));
+    ui->tYear->setValidator(new QRegExpValidator(regExpYear, this));
 }
 
 EditWindow::~EditWindow()
 {
     delete ui;
 }
+
+
