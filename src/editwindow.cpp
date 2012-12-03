38360d26838490a4e7c68a56430d764c42a97887
dajvido <jedynywierny@gmail.com>
2012-12-03 22:44:00 +0100

    #12 Corected names of objects

--- src/editwindow.cpp
+++ src/editwindow.cpp
@@ -13,17 +13,17 @@ EditWindow::EditWindow(QWidget *parent) :
     ui->setupUi(this);
 
     QRegExp regExpDay("0[1-9]|[12][0-9]|3[01]");
-    ui->fDay->setValidator(new QRegExpValidator(regExpDay, this));
-    ui->tDay->setValidator(new QRegExpValidator(regExpDay, this));
+    ui->fromDay->setValidator(new QRegExpValidator(regExpDay, this));
+    ui->toDay->setValidator(new QRegExpValidator(regExpDay, this));
 
     QRegExp regExpMonth("0[1-9]|1[012]");
-    ui->fMonth->setValidator(new QRegExpValidator(regExpMonth, this));
-    ui->tMonth->setValidator(new QRegExpValidator(regExpMonth, this));
+    ui->fromMonth->setValidator(new QRegExpValidator(regExpMonth, this));
+    ui->toMonth->setValidator(new QRegExpValidator(regExpMonth, this));
 
     QString stringYear = QDateTime::currentDateTime().toString("yyyy");
     QRegExp regExpYear(stringYear);
-    ui->fYear->setValidator(new QRegExpValidator(regExpYear, this));
-    ui->tYear->setValidator(new QRegExpValidator(regExpYear, this));
+    ui->fromYear->setValidator(new QRegExpValidator(regExpYear, this));
+    ui->toYear->setValidator(new QRegExpValidator(regExpYear, this));
 }
 
 EditWindow::~EditWindow()
