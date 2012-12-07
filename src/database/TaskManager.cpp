7555e4f8172a19be9ff933993b03370c025a4d3d
Dawid Drozd <dawid@dawid-LIFEBOOK-AH531.(none)>
2012-12-07 14:21:04 +0100

    Fixing TaskManager and adding new fields

--- src/database/TaskManager.cpp
+++ src/database/TaskManager.cpp
@@ -17,11 +17,15 @@ bool TaskManager::addTask(Task *task)
     query.append(DatabaseManager::TASK_DATABASE_NAME);
     query.append("(");
     query.append(DatabaseManager::TASK_NAME);
+    query.append(",").append(DatabaseManager::TASK_START_TIME);
+    query.append(",").append(DatabaseManager::TASK_END_TIME);
+    query.append(",").append(DatabaseManager::TASK_DESCRIPTION);
     query.append(") VALUES (");
     query.append(task->getName());
+    query.append(",").append(QString::number(task->getStartTime().toMSecsSinceEpoch()));
+    query.append(",").append(QString::number(task->getEndTime().toMSecsSinceEpoch()));
+    query.append(",").append(task->getDescription());
     query.append(")");
-
-   // "INSERT INTO names (id, test_field) VALUES (1, 'тест1')"
     dataBase->exec(query);
     return true;
 }
